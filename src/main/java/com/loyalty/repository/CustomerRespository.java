581a1d6c177b47691ff065ac916363a905c264bd
Renukeswar <renu.aws.learner@gmail.com>
2023-02-25 10:19:53 +0530

    customer QR Code while placing order

--- src/main/java/com/loyalty/repository/CustomerRespository.java
+++ src/main/java/com/loyalty/repository/CustomerRespository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @Repository
 public interface CustomerRespository extends JpaRepository<Customer,Long> {
+    
     Customer findByUsernameAndPassword(String username, String password);
 
     @Query(nativeQuery = true)
